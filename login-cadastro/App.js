1533f6a393c4865c9ab3dfaaa2c8c2414284577b
eduardoirine <151874030+eduardoirine@users.noreply.github.com>
2024-05-24 15:44:55 -0300

    ed

--- login-cadastro/App.js
+++ login-cadastro/App.js
@@ -2,52 +2,77 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
-const app = express();
-const port = 3000;
 
 const db = mysql.createConnection({
     host:'localhost',
-    user: 'root',
-    password:'',
-    database: 'login'
+    user:'root',
+    password: '',
+    database: 'Login'
 });
 
 db.connect((error)=>{
-    if (error){
-        console.log("Erro ao conectar com o banco de dados")
-    } else {
-        console.log("Conectado ao MySQL")
+    if(error){
+        console.log('Erro ao conectar com o MySQL', error);
+    }else{
+        console.log('Conectado ao MySQL')
     }
 });
 
-app.use(bodyParser.urlencoded({extended:true}))
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended:true}))
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + '/login.html')
+})
+
+app.get("/cadastro", (req, res)=>{
+  res.sendFile(__dirname + '/cadastro.html')
+})
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query('SELECT password FROM user WHERE username = ?', [username], (error, results)=>{
+      if(error){
+        console.error("Erro ao executar QUERY", error);
+        res.redirect('/erroquery');
+      } else if(results.length>0){
+        const passwordBD = results[0].password;
+        if(password === passwordBD){
+          console.log('Bem vindo')
+          res.redirect('/bemVindo.html')
+        }else {
+          console.log('Senha Incorreta')
+          res.redirect('/senhaerrada.html')
+        }
+      } else {
+        ('Usuario não existe')
+      }
+});
 });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    db.query('SELECT password FROM user WHERE username = ?', [username], (error, results) => {
-      if(error){
-        res.status(500).send('Erro ao obter usuários')
-      } else {
-        if(results.length > 0){ // Verifica se há resultados
-          const user = results[0]; // Obtém o primeiro resultado
-          if(user.password === password){
-             res.send(`Login bem-sucedido! Bem-vindo, ${username}.`);
-          } else {
-            res.status(401).send('Senha incorreta. Tente novamente.');
-          }
-        } else {
-          res.status(401).send('Este usuário não existe');
-        }
-      }
-    })
-  });
+app.post("/cadastro", (req, res)=>{
+  const username = req.body.username
+  const password = req.body.password
+  const confirm = req.body.passwordComfirm
 
+  if (password === confirm){
+    db.query('insert into user (username, password) values (?, ?)', [username, password], (error, results) => {
+      if(error){
+        console.log("Erro ao realizar o cadastro")
+      }else{
+        console.log("Cadastro realizado com sucesso")
+      }
+    });
+  } else{
+    console.log('Senhas não coincidem')
+  }
+
+})
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando no endereço: http://localhost:${port}`)
-});
+})
